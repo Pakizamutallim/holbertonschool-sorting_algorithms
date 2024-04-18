0b640ea2abb414461b6d001cd60a67bdb78e987f
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-18 13:25:59 +0000

    added prototype

--- sort.h
+++ sort.h
@@ -26,4 +26,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void swap_ints(int *a, int *b);
+
 #endif

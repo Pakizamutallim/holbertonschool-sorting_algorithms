8767df1639155aa39d5046b9dcbd229ec52b50a9
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-18 11:47:14 +0000

    created first file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+
+/**
+ * _swap - Swaps two nodes of doubly linked list
+ *
+ * @node: node base to change
+ * @list: double link list head
+ *
+ * Return: No Return
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	listint_t *tmp;
+
+	current = *list;
+	current = current->next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	while (current != NULL)
+	{
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			tmp = current;
+
+			if (current->next != NULL)
+				current->next->prev = tmp->prev;
+			current->prev->next = tmp->next;
+			current = current->prev;
+			tmp->prev = current->prev;
+			tmp->next = current;
+			if (current->prev != NULL)
+				current->prev->next = tmp;
+			current->prev = tmp;
+
+			if (tmp->prev == NULL)
+				*list = tmp;
+
+			print_list(*list);
+			current = current->prev;
+		}
+		current = current->next;
+	{
+}

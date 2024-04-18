3db1ea78acadc711149166cbde2b0b875604af08
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-18 12:09:44 +0000

    created 3rd file

---
+++ 3-quick_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * quick_sort -  sorts an array of integers in ascending order using the
+ *
+ * @array: data to sort
+ * @size: size of data
+ *
+ * Return: 0
+ */
+
+void quick_sort(int *array, size_t size)
+{
+}

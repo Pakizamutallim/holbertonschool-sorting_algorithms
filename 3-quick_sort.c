4784e76c1a1610d7eeaadbb671ee2aa4b5f7e52b
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-19 12:53:04 +0000

    line 59 error fixed

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -54,6 +54,8 @@ int lomuto_partition(int *array, size_t size, int left, int right)
 {
 	int *pivot, above, below;
 
+	pivot = array[right]
+
 	for (above = below = left; below < right; below++)
 	{
 		if (array[below] < *pivot)

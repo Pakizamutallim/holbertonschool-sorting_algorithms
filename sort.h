1b39912dcc60cdcb2a4b8716b7dcfaa8584c7957
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-20 11:47:55 +0000

    added new prototype adn deleted unnecessary

--- sort.h
+++ sort.h
@@ -18,16 +18,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-int lomuto_partition(int *array, int lower, int higher, size_t size);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void swap_ints(int *a, int *b);
+void print_array(const int *array, size_t size);
 
 #endif
